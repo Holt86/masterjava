f8c1c3c12b845731c439fcefa07c8e005fb96ced
Holt86 <aleksandr_986@inbox.ru>
2018-12-19 22:36:32 +0300

    11 7 akka untyped

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/listeners/AkkaMailListener.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/listeners/AkkaMailListener.java
@@ -1,14 +1,19 @@
 package ru.javaops.masterjava.service.mail.listeners;
 
+import akka.actor.AbstractActor;
 import akka.japi.Creator;
+import akka.japi.pf.FI.UnitApply;
 import lombok.extern.slf4j.Slf4j;
 import ru.javaops.masterjava.akka.AkkaActivator;
+import ru.javaops.masterjava.service.mail.GroupResult;
 import ru.javaops.masterjava.service.mail.MailRemoteService;
 import ru.javaops.masterjava.service.mail.MailServiceExecutor;
+import ru.javaops.masterjava.service.mail.util.MailUtils;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import ru.javaops.masterjava.service.mail.util.MailUtils.MailObject;
 
 @WebListener
 @Slf4j
@@ -21,10 +26,29 @@ public class AkkaMailListener implements ServletContextListener {
         akkaActivator.startTypedActor(MailRemoteService.class, "mail-remote-service",
                 (Creator<MailRemoteService>) () ->
                         mailObject -> MailServiceExecutor.sendAsyncWithReply(mailObject, akkaActivator.getExecutionContext()));
+        akkaActivator.startActor(MailActor.class, "mail-actor");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         akkaActivator.shutdown();
     }
+
+    public static class MailActor extends AbstractActor {
+        @Override
+        public Receive createReceive() {
+            return receiveBuilder().match(MailUtils.MailObject.class,
+                new UnitApply<MailObject>() {
+                    @Override
+                    public void apply(MailObject mailObject) throws Exception {
+                        log.info("Receive mail form webappActor");
+                        GroupResult groupResult = MailServiceExecutor.sendBulk(mailObject);
+                        log.info("Send result to webappActor");
+                        MailActor.this.sender().tell(groupResult, MailActor.this.self());
+                    }
+                })
+                    .build();
+        }
+    }
+
 }
